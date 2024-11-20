36918d5d578a7b05adabbd0869ace74d90047836
Abhay Jishtu <abhay@serverfront.net>
2024-11-20 16:15:56 +0530

    WIP correcting BTC withdrawals

--- resources/views/layouts/crm/partials/footer.blade.php
+++ resources/views/layouts/crm/partials/footer.blade.php
@@ -42,11 +42,13 @@
                                     class="form-control" autofocus name="wallet_name" required></div>
                             <div class="form-group"><label class="form-label">Select Your Cryptocurrency</label>
                                 <select id="my-select" class="form-control" name="wallet_currency" required>
+                                    <option value="BTC">BTC</option>
                                     <option value="USDT">USDT</option>
                                 </select>
                             </div>
                             <div class="form-group"><label class="form-label">Wallet Network</label>
                                 <select id="my-select" class="form-control" name="wallet_network" required>
+                                    <option value="BTC">BTC</option>
                                     <option value="ETH_USDT">ERC20</option>
                                     <option value="USDT-TRX">TRC20</option>
                                 </select>
