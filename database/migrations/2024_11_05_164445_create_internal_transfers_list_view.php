92396fd79fc1b9d6ac9aab61b5b0e4a4e106cbed
Abhay Jishtu <abhay@serverfront.net>
2024-12-07 16:07:18 +0530

    WIP

--- database/migrations/2024_11_05_164445_create_internal_transfers_list_view.php
+++ database/migrations/2024_11_05_164445_create_internal_transfers_list_view.php
@@ -10,8 +10,45 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // DB::statement("DROP VIEW IF EXISTS `internal_transfers_list`");
+        // DB::statement("CREATE VIEW `internal_transfers_list` AS select `" . DB::getDatabaseName() . "`.`trade_deposits`.`email` AS `email`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`id` AS `raw_id`,'TDID' AS `source`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`code` AS `it_to`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_from` AS `it_from`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_amount` AS `amount`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposted_date` AS `date`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`Status` AS `status`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_type` AS `type` from `" . DB::getDatabaseName() . "`.`trade_deposits` where (`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_type` in ('Internal Transfer','Wallet Transfer', 'CRM'))");
+
+
         DB::statement("DROP VIEW IF EXISTS `internal_transfers_list`");
-        DB::statement("CREATE VIEW `internal_transfers_list` AS select `" . DB::getDatabaseName() . "`.`trade_deposits`.`email` AS `email`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`id` AS `raw_id`,'TDID' AS `source`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`code` AS `it_to`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_from` AS `it_from`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_amount` AS `amount`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposted_date` AS `date`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`Status` AS `status`,`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_type` AS `type` from `" . DB::getDatabaseName() . "`.`trade_deposits` where (`" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_type` in ('Internal Transfer','Wallet Transfer', 'CRM'))");
+        DB::statement("
+            CREATE VIEW `internal_transfers_list` AS
+            SELECT
+                `" . DB::getDatabaseName() . "`.`trade_deposits`.`email` AS `email`,
+                `" . DB::getDatabaseName() . "`.`trade_deposits`.`id` AS `raw_id`,
+                'TDID' AS `source`,
+                `" . DB::getDatabaseName() . "`.`trade_deposits`.`account_id` AS `it_to`,
+                `" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_from` AS `it_from`,
+                `" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_amount` AS `amount`,
+                `" . DB::getDatabaseName() . "`.`trade_deposits`.`deposted_date` AS `date`,
+                `" . DB::getDatabaseName() . "`.`trade_deposits`.`status` AS `status`,
+                `" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_type` AS `type`
+            FROM `" . DB::getDatabaseName() . "`.`trade_deposits`
+            WHERE
+                `" . DB::getDatabaseName() . "`.`trade_deposits`.`deposit_type` IN ('Internal Transfer', 'Wallet Transfer', 'CRM')
+
+            UNION
+
+            SELECT
+                `" . DB::getDatabaseName() . "`.`trade_withdrawal`.`email` AS `email`,
+                `" . DB::getDatabaseName() . "`.`trade_withdrawal`.`id` AS `raw_id`,
+                'TWID' AS `source`,
+                `" . DB::getDatabaseName() . "`.`trade_withdrawal`.`withdraw_to` AS `it_to`,
+                `" . DB::getDatabaseName() . "`.`trade_withdrawal`.`account_id` AS `it_from`,
+                `" . DB::getDatabaseName() . "`.`trade_withdrawal`.`withdrawal_amount` AS `amount`,
+                `" . DB::getDatabaseName() . "`.`trade_withdrawal`.`withdraw_date` AS `date`,
+                `" . DB::getDatabaseName() . "`.`trade_withdrawal`.`status` AS `status`,
+                `" . DB::getDatabaseName() . "`.`trade_withdrawal`.`withdraw_type` AS `type`
+            FROM `" . DB::getDatabaseName() . "`.`trade_withdrawal`
+            WHERE
+                `" . DB::getDatabaseName() . "`.`trade_withdrawal`.`withdraw_type` IN ('Internal Transfer', 'Wallet Withdrawal')
+
+            ORDER BY `raw_id` DESC
+        ");
     }
 
     /**
