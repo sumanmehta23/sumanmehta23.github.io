016cfae174f0edec008179e177bb1eae2dea7a37
Digvijay Singh <singh@serverfront.net>
2024-12-07 17:10:35 +0530

    WIP

--- app/Http/Controllers/Transactions.php
+++ app/Http/Controllers/Transactions.php
@@ -17,18 +17,13 @@ class Transactions extends Controller
     public function index()
     {
         $email = $email = auth()->user()->email;
-        $deposit_history = TradeDeposit::with('liveAccount.accountType')
-            ->where('user_id',  auth()->user()->id)
-            ->where('deposit_type', 'CryptoChill')
-            ->orderBy('id', 'desc')
-            ->get();
-        $wallet_deposit_history = WalletDeposit::with('liveAccount.accountType')
+        
+        $deposit_history = WalletDeposit::with('liveAccount.accountType')
             ->where('user_id',  auth()->user()->id)
             ->where('deposit_type', 'CryptoChill')
             ->orderBy('id', 'desc')
             ->get();
             // dd($wallet_deposit_history);
-            $deposit_history = $deposit_history->merge($wallet_deposit_history);
         // Fetching withdrawal history
         $withdrawal_history = WalletWithdraw::where('email', $email)
             ->where('withdraw_type', 'Wallet Withdrawal')
