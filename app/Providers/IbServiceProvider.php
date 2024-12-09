e07dbc1ed64efb40ffde9894428ca3a0663960a5
Digvijay Singh <singh@serverfront.net>
2024-12-09 16:00:53 +0530

    WIP

--- app/Providers/IbServiceProvider.php
+++ app/Providers/IbServiceProvider.php
@@ -1,19 +1,28 @@
 <?php
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
-use App\Http\Controllers\IbController;
 use App\Models\Ib1;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Cache;
+use App\Http\Controllers\IbController;
+use Illuminate\Support\ServiceProvider;
 
 class IbServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         View::composer('*', function ($view) {
-            $email = session('clogin');
-            if ($email) {
-                $ibResult = Ib1::where('email', $email)->first();
+            $user = auth()->user();
+            if ($user) {
+                $cacheKey = 'ib1_1' ;
+
+                if (!Cache::has($cacheKey)) {
+                    $ibResult =Ib1::where('user_id', $user->id)->first();
+                    Cache::put($cacheKey, $ibResult, 60);
+                } else {
+                    $ibResult=Cache::get($cacheKey);
+                }
                 $view->with('ibResult', $ibResult);
             } else {
                 $view->with('ibResult', null);
