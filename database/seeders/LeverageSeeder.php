254a4960b54b97f5acfde105f573242df80f8953
Digvijay Singh <singh@serverfront.net>
2024-12-13 12:23:44 +0530

    WIP

--- database/seeders/LeverageSeeder.php
+++ database/seeders/LeverageSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\AccountType;
 use App\Models\Leverage;
+use App\Models\AccountType;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\File;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -20,12 +21,19 @@ class LeverageSeeder extends Seeder
 
          // Insert each record into the 'leverage' table
          foreach ($leverageData as $leverage) {
-            Leverage::create([
-                'account_type_id' => AccountType::where('ac_index',$leverage['account_type_id'])->value('id'),
-                'account_leverage' => $leverage['account_leverage'],
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+            try {
+                Leverage::create([
+                    'account_type_id' => AccountType::where('ac_index',$leverage['account_type_id'])->value('id'),
+                    'account_leverage' => $leverage['account_leverage'],
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            } catch (\Throwable $th) {
+                Log::error($th->getMessage(). json_encode($leverage));
+                
+
+            }
+           
         }
     }
 }
