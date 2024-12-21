d852d05b5a8d8741f5bb4d60d596a4d3d13e070d
Abhay Jishtu <abhay@serverfront.net>
2024-12-21 10:42:17 +0530

    WIP

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -230,7 +230,7 @@
                     </thead>
                     <tbody>
                       @foreach ($internal_transfer as $history)
-                      {{ dd($internal_transfer); }}
+                      {{ dump($internal_transfer); }}
                         @php
                             if ($history->type == 'CRM') {
                                 $from = 'CRM';
