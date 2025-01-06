865bf931de51539c33768c2776fd61b072405910
Digvijay Singh <singh@serverfront.net>
2025-01-06 18:48:53 +0530

    WIP

--- routes/web.php
+++ routes/web.php
@@ -268,6 +268,6 @@ Route::prefix("/admin")->name("admin.")->group(function () {
         Route::get("/search", [SearchController::class, 'index']);
 
 
-        Route::get("/sendMarketEmail", [Dashboard::class, 'sendMarketingEmail']);
+        // Route::get("/sendMarketEmail", [Dashboard::class, 'sendMarketingEmail']);
     });
 });
