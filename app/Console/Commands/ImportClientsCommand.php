562c8e936cf842f2136076df3191a58de13aa636
Digvijay Singh <singh@serverfront.net>
2025-01-09 13:27:23 +0530

    WIP

---
+++ app/Console/Commands/ImportClientsCommand.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use League\Csv\Reader;
+use App\Models\Country;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class ImportClientsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:import-clients-command {file : The path to the CSV file}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Parse a CSV file and output its content';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $filePath = $this->argument('file');
+        $missingcountries=0;
+        // Validate the file
+        if (!file_exists($filePath) || !is_readable($filePath)) {
+            $this->error("The file {$filePath} does not exist or is not readable.");
+            return Command::FAILURE;
+        }
+
+        try {
+            // Read the CSV file
+            $csv = Reader::createFromPath($filePath, 'r');
+            $csv->setHeaderOffset(0); // The first row will be treated as the header
+
+            $records = $csv->getRecords();
+
+            foreach ($records as $index => $record) {
+
+                $country=Country::where('country_name',$record['Where are you from?'])->first();
+                if(!$country){
+                    $missingcountries++;
+                    $this->error("Row {$record['Where are you from?']}: Country not found");
+                    continue;
+                }
+                // dd($country);
+                $phone=str_replace("'","",$record['Your phone number']);
+                // $this->info("Row {$index}:");
+                // $this->line("Full Name: " . $record['Full Name']);
+                // $this->line("Email: " . $record['Your email address']);
+                // $this->line("Phone: " . $phone);
+                // $this->line("Location: " . $record['Where are you from?']);
+                // $this->line("Request: " . $record['What do you want?']);
+                // $this->line('---');
+                $existing=User::where('email',$record['Your email address'])->first();
+                if($existing){
+                    if(empty( $existing->ib1) || $existing->ib1=='noIB'){
+                        $existing->ib1='Swingtradinglab';
+                        
+                        // $existing->save();
+                    }else{
+                        $this->error("Row {$record['Your email address']}: User already exists and has an IB1");
+                    }
+                    continue;
+                }else{
+                    // User::create([
+                    //     'fullname'=>$record['Full Name'],
+                    //     'email'=>$record['Your email address'],
+                    //     'username'=>$record['Your email address'],
+                    //     'password'=>Hash::make('password'.rand(999,999999)),
+                    //     'country_code'=>$country->country_code,
+                    //     'number'=>$phone,
+                    //     'country'=>$record['Where are you from?'],
+                    //     // 'request'=>$record['What do you want?'],
+                    //     'wallet_enabled'=>1,
+                    //     'ib1'=>'Swingtradinglab'
+                    // ]);
+                }
+                // die();
+            }
+        } catch (\Exception $e) {
+            $this->error('An error occurred while parsing the CSV file: ' . $e->getMessage());
+            return Command::FAILURE;
+        }
+
+        $this->info('CSV parsing completed successfully. Ignored '.$missingcountries.' rows with missing countries.');
+        return Command::SUCCESS;
+    }
+}
