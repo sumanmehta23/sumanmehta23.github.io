dcd5e7ca6c36d41e07330bcaddda61a21d6ac562
Abhay Jishtu <abhay@serverfront.net>
2025-01-10 10:55:58 +0530

    WIP

--- app/Http/Controllers/Admin/MT5Controller.php
+++ app/Http/Controllers/Admin/MT5Controller.php
@@ -248,7 +248,7 @@ class MT5Controller extends Controller
             // $comment = $description;
             $comment = $type === 'in' ? 'Bonus Deposit' : 'Bonus Withdraw';;
             $ticket = null;
-            if($login==471399){
+            if($login==471333399){
                 $operation=MTEnDealAction::DEAL_BONUS;
             }else{
                 $operation=MTEnDealAction::DEAL_BALANCE;
