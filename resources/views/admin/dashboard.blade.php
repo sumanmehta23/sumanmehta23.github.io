0f63a96f37484076b075eae576edc120f1802b8e
Digvijay Singh <singh@serverfront.net>
2025-01-13 11:47:50 +0530

    WIP

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,26 +16,25 @@
 
             <div class="row">
                 @can('dashboard:viewTotalDeposit')
-                    
-                
-                <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                    <div class="card custom-card">
-                        <div class="card-body">
-                            <div class="card-order">
-                                <h6 class="mb-2">Total Deposit</h6>
-                                <h2 class="text-end "><i
-                                        class="mdi mdi-wallet icon-size float-start text-primary text-primary-shadow"></i><span>$
-                                        {{ number_format($trade_deposit->deposit + $wallet_deposit->deposit,2) }}</span>
-                                </h2></span>
-                                <p class="mb-0">Trading Deposit<span class="float-end">${{ number_format($trade_deposit->deposit,2) }}
-                                    </span></p>
-                                <p class="mb-0">Wallet Deposit<span
-                                        class="float-end">${{ number_format($wallet_deposit->deposit,2) }}</span></p>
+                    <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                        <div class="card custom-card">
+                            <div class="card-body">
+                                <div class="card-order">
+                                    <h6 class="mb-2">Total Deposit</h6>
+                                    <h2 class="text-end "><i
+                                            class="mdi mdi-wallet icon-size float-start text-primary text-primary-shadow"></i><span>$
+                                            {{ number_format($trade_deposit->deposit + $wallet_deposit->deposit,2) }}</span>
+                                    </h2></span>
+                                    <p class="mb-0">Trading Deposit<span class="float-end">${{ number_format($trade_deposit->deposit,2) }}
+                                        </span></p>
+                                    <p class="mb-0">Wallet Deposit<span
+                                            class="float-end">${{ number_format($wallet_deposit->deposit,2) }}</span></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div><!-- COL END -->
+                    </div><!-- COL END -->
                 @endcan
+                @can("wallet_withdraw:viewAny")
                 <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card custom-card">
                         <div class="card-body">
@@ -52,6 +51,8 @@
                         </div>
                     </div>
                 </div><!-- COL END -->
+                @endcan
+                @can('client:viewAny')
                 <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card custom-card">
                         <div class="card-body">
@@ -68,9 +69,11 @@
                         </div>
                     </div>
                 </div><!-- COL END -->
+                @endcan
             </div>
             <!-- ROW-1 END -->
             <div class="row">
+                @can("wallet_deposit:viewAny")
                 <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
                     <div class="card bg-primary img-card box-primary-shadow">
                         <div class="card-body">
@@ -85,6 +88,8 @@
                         </div>
                     </div>
                 </div><!-- COL END -->
+                @endcan
+                @can("wallet_withdraw:viewAny")
                 <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
                     <div class="card bg-secondary img-card box-secondary-shadow">
                         <div class="card-body">
@@ -99,6 +104,8 @@
                         </div>
                     </div>
                 </div>
+                @endcan
+                @can("ib:viewAny")
                 <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
                     <div class="card bg-success img-card box-success-shadow">
                         <div class="card-body">
@@ -112,6 +119,8 @@
                         </div>
                     </div>
                 </div><!-- COL END -->
+                @endcan
+                @can("client:viewAny")
                 <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
                     <div class="card bg-info img-card box-info-shadow">
                         <div class="card-body">
@@ -126,8 +135,10 @@
                         </div>
                     </div>
                 </div><!-- COL END -->
+                @endcan
             </div>
             <!-- Row -->
+            @can("wallet_deposit:viewAny")
             <div class="row row-sm">
                 <div class="col-lg-12">
                     <div class="card custom-card product-sales">
@@ -308,8 +319,8 @@
                     </div>
                 </div>
             </div>
-
-
+            @endcan
+            @can("wallet_withdraw:viewAny")
             <div class="row row-sm">
                 <div class="col-lg-12">
                     <div class="card custom-card product-sales">
@@ -500,7 +511,7 @@
                 </div>
             </div>
             <!-- End Row -->
-
+            @endcan
         </div>
     </div>
 @endsection
