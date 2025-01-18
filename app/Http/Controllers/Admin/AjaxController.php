dd40dbbee5f9687fa7403a18f4a24f6f5e48d630
Piyasha <piyasha@techsprinters.com>
2025-01-18 17:33:42 +0530

    WIP

--- app/Http/Controllers/Admin/AjaxController.php
+++ app/Http/Controllers/Admin/AjaxController.php
@@ -807,13 +807,22 @@ class AjaxController extends Controller
                 ->addColumn('account_group', function($row){
                     return $row->accountType->ac_group;
                 })
+                ->addColumn('actions', function($row){
+                    $html = "";
+                    $html .= "<a class='deleteAcc' data-enc='{$row->id}'>
+                    <span class='badge text-danger' data-bs-toggle='tooltip'>
+                        <svg  xmlns='http://www.w3.org/2000/svg'  width='24'  height='24'  viewBox='0 0 24 24'  fill='none'  stroke='currentColor'  stroke-width='2'  stroke-linecap='round'  stroke-linejoin='round'  class='icon icon-tabler icons-tabler-outline icon-tabler-trash'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M4 7l16 0' /><path d='M10 11l0 6' /><path d='M14 11l0 6' /><path d='M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12' /><path d='M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3' /></svg>
+                    </span>
+                  </a>";
+                  return $html;
+                })
                 ->addColumn('created_date', function($row){
                     return date('Y-m-d', strtotime($row->created_at));
                 })
                 ->addColumn('created_time', function($row){
                     return date('H:i:s', strtotime($row->created_at));
                 })
-                ->rawColumns(['email', 'code', 'leverage', 'balance', 'created_at','fullname','fullemail'])
+                ->rawColumns(['email', 'code', 'leverage', 'balance', 'created_at','fullname','fullemail', 'actions'])
                 ->make(true);
         }
 
--- app/Http/Controllers/Admin/ClientAccController.php
+++ app/Http/Controllers/Admin/ClientAccController.php
@@ -8,10 +8,16 @@ use App\Models\Account;
 use App\Models\AccountType;
 use App\Models\RelationshipManager;
 use App\Models\User;
+use App\Services\MailService;
 use DB;
 
 class ClientAccController extends Controller
 {
+    protected $mailService;
+    public function __construct(MailService  $mailService)
+    {
+        $this->mailService = $mailService;
+    }
     public function live_accounts()
     {
         // $role = session('userData')['userRole'];
@@ -86,5 +92,55 @@ class ClientAccController extends Controller
     public function requested_accounts()
     {
         return view('admin.client_accounts.requested_accounts');
+    }
+    public function deleteAccounts(Request $request)
+    {
+        // dd($request->all());
+        $settings = settings();
+
+        $validatedData = $request->validate([
+            // 'id' => 'required|id',
+            'email' => 'required|email'
+        ]);
+
+        $account = Account::where('id', $request->id)->first();
+
+        if ($account) {
+           
+            // $account->delete();
+
+            // $date = date('Y-m-d', strtotime($account_details->created_at));
+
+            $email = $validatedData['email'];
+            $type = $account->demo == "1"? "Demo account": "Live account";
+
+            $from = $settings['email_from_address'];
+            $transid = "WDID" . str_pad($account->id, 4, '0', STR_PAD_LEFT);
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            $headers .= 'From:' . $settings['admin_title'] . '<' . $from . '>' . "\r\n";
+            $emailSubject = $settings['admin_title'] . ' - Transaction Approved';
+            $content = '<div>We are pleased to inform you that your account has been deleted.</div>
+                        <div><b>Account code: </b>' . $account->code . '</div>
+                        <div><b>Account type: </b>' .  $type . '</div>
+                        <div><b>Created Date: </b>' . $account->created_at . '</div>
+                        <div><b>Deleted Date: </b>' . $account->deleted_at . '</div>';
+            $templateVars = [
+                'name' => $account->name,
+                'site_link' => $settings['copyright_site_name_text'],
+                'email' => $settings['email_from_address'],
+                'content' => $content,
+                'title_right' => 'Account',
+                'subtitle_right' => 'Deleted',
+                'btn_text' => 'Go To Dashboard',    
+            ];
+            $this->mailService->sendEmail($email, $emailSubject, $headers, '', $templateVars);
+    
+            return redirect()->back()->with('success', 'Account deleted successfully.');
+        } else {
+           
+            return redirect()->back()->with('error', 'Account not found.');
+        }
+        
     }
 }
