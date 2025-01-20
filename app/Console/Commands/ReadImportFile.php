0b192059560ea6f14700ff2d23fe23b97da4bbac
Digvijay Singh <singh@serverfront.net>
2025-01-20 19:26:06 +0530

    WIP

---
+++ app/Console/Commands/ReadImportFile.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ReadImportFile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * Group_Code_Alex_Yes_Bonus.csv
+     * @var string
+     */
+    protected $signature = 'app:read-import-file {file}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        ini_set("max_execution_time", 0);
+        $file = $this->argument('file');
+        $file = fopen($file, 'r');
+        $header = fgetcsv($file);
+        $data = [];
+        while ($row = fgetcsv($file)) {
+            $data[] = array_combine($header, $row);
+          
+            Artisan::queue('app:update-leverage',[
+                'account_code'=>$row[3],
+                'leverage'=>10
+            ]);
+            //print last command output
+            echo Artisan::output();
+        }
+        fclose($file);
+    }
+}
---
+++ app/Console/Commands/UpdateLeverage.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\MT5\MTRetCode;
+use App\Models\Account;
+use App\Services\MT5Service;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class UpdateLeverage extends Command
+{
+    protected $api;
+    protected $mailService;
+    protected $mt5Service;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    //It accepts account code and leverage as arguments
+    protected $signature = 'app:update-leverage {account_code} {leverage}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+    function __construct(MT5Service $mt5Service)
+    {
+        $this->mt5Service = $mt5Service;
+        $this->mt5Service->connect();
+        $this->api = $this->mt5Service->getApi();
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $account_code = $this->argument('account_code');
+        $leverage = $this->argument('leverage');
+        if (($error_code = $this->api->UserGet($account_code, $trade_user)) != MTRetCode::MT_RET_OK) {
+            Log::error('error'.' Something went wrong on getting user  details' . MTRetCode::GetError($error_code));
+        }
+        if (($error_code = $this->api->PositionGetTotal($account_code, $total_positions)) != MTRetCode::MT_RET_OK) {
+            Log::error('error'. ' Something went wrong on Updating details' . MTRetCode::GetError($error_code));
+            // return redirect()->back()->with('error', 'Something went wrong on Updating details' . MTRetCode::GetError($error_code));
+        }
+
+        if($total_positions < 2){
+            $trade_user->Leverage = $leverage;
+
+            $error_code = $this->api->UserUpdate($trade_user, $updated_user);
+                if ($error_code != MTRetCode::MT_RET_OK) {
+                    Log::error("error ". $account_code." Something went wrong on Updating details" . MTRetCode::GetError($error_code));
+                    $this->info($account_code.":".$total_positions);
+                } else {
+                    Account::where('code', $account_code)->update([
+                        'leverage' => $leverage
+                    ]);
+                }
+        }else{
+            $this->info($account_code.":".$total_positions);
+        }
+    }
+}
