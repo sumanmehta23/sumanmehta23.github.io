28c9b7c787c2e713d08930c792ed2eede023f44d
Abhay Jishtu <abhay@serverfront.net>
2025-01-24 16:47:22 +0530

    WIP

--- resources/views/trade_deposit.blade.php
+++ resources/views/trade_deposit.blade.php
@@ -133,7 +133,7 @@
                                                         <form method="post" id="tradeDepositForm">
                                                             @csrf
                                                             <input type="hidden" name="user[email]"
-                                                                value="{{ session('clogin') }}" required
+                                                                value="{{ session('user')->email }}" required
                                                                 class="form-control fill">
                                                             <input class="user_code" type="hidden"
                                                                 name="user[account_id]" value="" readonly required>
