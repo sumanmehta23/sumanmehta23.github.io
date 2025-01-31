c40656e9ba3cafccf956a69cf71195c3d891d959
Abhay Jishtu <abhay@serverfront.net>
2025-01-31 11:43:50 +0530

    WIP

--- app/Http/Controllers/Admin/SumsubController.php
+++ app/Http/Controllers/Admin/SumsubController.php
@@ -116,10 +116,12 @@ class SumsubController extends Controller
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_ENCODING => "",
           CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => "GET",
           CURLOPT_HTTPHEADER => $headers,
+          CURLOPT_POSTFIELDS => '',
         ]);
 
         $response = curl_exec($curl);
