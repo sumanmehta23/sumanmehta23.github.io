a999bbb3ef8001a49bf9e21f11d20f6058b9cdda
Abhay Jishtu <abhay@serverfront.net>
2025-01-31 17:00:08 +0530

    WIP

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -56,12 +56,12 @@ class LoginController extends Controller
 
          // Find the user by email
          $user = User::where('email', $request->input('email'))->where('email_confirmed', 1)->first();
-         dd($user);
+
          // Check if user exists
          if (!$user) {
              return redirect()->back()->with('error', 'Your login details are invalid or your email is not verified.');
          }
-
+         dd(Hash::needsRehash($user->password));
          // Check if the password is in plain text (not hashed)
          if (Hash::needsRehash($user->password)) {
              // If it's plain text, hash it and update the user's password
