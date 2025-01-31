84dd3992003a48a4c7762e08dbeae3e0ae9fe9e2
Abhay Jishtu <abhay@serverfront.net>
2025-01-31 17:08:27 +0530

    WIP

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -73,7 +73,7 @@ class LoginController extends Controller
                  return redirect()->back()->with('error', 'Your login details are invalid or your email is not verified.');
              }
          } else {
-            dd('ssss');
+            dd(Hash::check($request->input('password'), $user->password));
              // If password is hashed, verify it
              if (!Hash::check($request->input('password'), $user->password)) {
                  return redirect()->back()->with('error', 'Your login details are invalid or your email is not verified.');
