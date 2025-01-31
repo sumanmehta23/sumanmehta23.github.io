c76711f5dc044e2198f4840cd637d980fb04f198
Abhay Jishtu <abhay@serverfront.net>
2025-01-31 16:59:05 +0530

    WIP

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -53,10 +53,10 @@ class LoginController extends Controller
             'email' => 'required|email',
             'password' => 'required',
         ]);
-            dd($request->input('email'));
+
          // Find the user by email
          $user = User::where('email', $request->input('email'))->where('email_confirmed', 1)->first();
-
+         dd($user);
          // Check if user exists
          if (!$user) {
              return redirect()->back()->with('error', 'Your login details are invalid or your email is not verified.');
