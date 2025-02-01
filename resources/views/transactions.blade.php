dea34e3f3b53043029138bbbdf7505937127f414
Abhay Jishtu <abhay@serverfront.net>
2025-02-01 16:05:35 +0530

    WIP

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -192,9 +192,12 @@
                           <td>
                             <h6 class="f-w-500 f-16">${{ number_format($history->withdraw_transaction_fee, 2) }}</h6>
                           </td>
+                          {{-- {{ dump(($history->payout_req)) }} --}}
                           <td class="{{ $history->status == 0 ? 'text-warning' : ($history->status == 1 ? 'text-success' : 'text-danger') }}">
                             <p>{{ $history->status == 0 ? 'Pending' : ($history->status == 1 ? 'Success' : 'Cancelled') }}</p>
-                            <p>{{ $history->admin_remark ? '(' . $history->admin_remark . ')' : '' }}</p>
+                            <p>{{ ($history->payout_req != NULL) ?  htmlspecialchars(isset($history->payout_req) ? json_decode($history->payout_res, true)['reason'] : '') : ($history->admin_remark ? '(' . $history->admin_remark . ')' : '' )}}</p>
+
+
                           </td>
                           @if($history->status == 0)
                             <td >
