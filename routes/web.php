ddd90f3dca33d7f39b4024108d83e7a42612012e
Abhay Jishtu <abhay@serverfront.net>
2025-02-03 10:18:17 +0530

    WIP

--- routes/web.php
+++ routes/web.php
@@ -298,8 +298,8 @@ Route::prefix("/admin")->name("admin.")->group(function () {
             Route::get("/liveAccounts", [ClientAccController::class, 'live_accounts'])->name('liveAccounts')->middleware('check.permissions:account:viewLiveAccounts');
             Route::get("/demoAccounts", [ClientAccController::class, 'demo_accounts'])->name('demoAccounts')->middleware('check.permissions:account:viewDemoAccounts');
             Route::get("/requestedAccounts", [ClientAccController::class, 'requested_accounts'])->name('requestedAccounts')->middleware('check.permissions:account:viewRequestedAccounts');
-            Route::post('/activate_account', [MT5Accounts::class, 'updateLiveAccount'])->name('activate_account');
             Route::post("/deleteAccounts", [TradeWithdrawal::class, 'deleteAccounts'])->name('deleteAccounts')->middleware('check.permissions:account:viewLiveAccounts');
+            Route::post('/activate_account', [MT5Accounts::class, 'activateAccount'])->name('activate_account');
         });
 
         Route::prefix('/ui_settings')->group(function () {
