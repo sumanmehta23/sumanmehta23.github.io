9b8a3283d584b7bb74d7e7b3e3151a853996eda3
Abhay Jishtu <abhay@serverfront.net>
2025-03-11 09:23:40 +0530

    WIP

--- resources/views/trade_deposit.blade.php
+++ resources/views/trade_deposit.blade.php
@@ -169,7 +169,8 @@
                                                                                 <input name="user[deposit]"
                                                                                     id="deposit_amount" type="number"
                                                                                     class="form-control fill tradedeposit_amount"
-                                                                                    aria-label="Amount" required>
+                                                                                    aria-label="Amount" step="0.01"
+                                                                                    min="0.01" required>
                                                                             </div>
                                                                         </div>
                                                                     </div>
