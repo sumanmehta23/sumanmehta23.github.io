0c8a7deae84d9f190fa32ce1cacc6905f1531783
Abhay Jishtu <abhay@serverfront.net>
2025-04-11 16:08:14 +0530

    WIP

--- app/Http/Controllers/Wallet.php
+++ app/Http/Controllers/Wallet.php
@@ -1124,7 +1124,8 @@ class Wallet extends Controller
                 'email' => $from,
                 "content" => $content,
                 "title_right" => "Activate",
-                "subtitle_right" => "Your Wallet Withdrawal Request"
+                "subtitle_right" => "Your Wallet Withdrawal Request",
+                "btn_text" => "Verify"
             ];
 
             $this->mailService->sendEmail($toEmail, $emailSubject, $headers, '', $templateVars);
