c5593e56c01cbbe9e23f2b2325624760d2f8e813
Digvijay Singh <tech@lqhmarkets.com>
2025-04-18 09:05:07 +0530

    WIP

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('telescope:prune')->daily();
-        $schedule->command('app:sync-account-trades')->everyThirtyMinutes();
+        $schedule->command('app:sync-account-trades')->everyTwoHours();
     }
 
     /**
