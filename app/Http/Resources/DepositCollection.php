33f1f707676cc7a14168ce70bbc4b448c30f8281
Abhay Jishtu <abhay@serverfront.net>
2025-04-24 15:46:31 +0530

    WIP

--- app/Http/Resources/DepositCollection.php
+++ app/Http/Resources/DepositCollection.php
@@ -26,7 +26,7 @@ class DepositCollection extends ResourceCollection
                     'deposit_type' => $deposit->deposit_type,
                     'email' => $deposit->email,
                     'status' => $deposit->status,
-                    'created_at' => Carbon::parse($row->created_at)->addHours(3)->toDateTimeString(),
+                    'created_at' => Carbon::parse($deposit->created_at)->addHours(3)->toDateTimeString(),
                 ];
             }),
         ];
