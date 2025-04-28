095fd8930d6df243a11940b1b3877976accf3822
Abhay Jishtu <abhay@serverfront.net>
2025-04-28 16:50:12 +0530

    WIP

--- app/Http/Controllers/Payment.php
+++ app/Http/Controllers/Payment.php
@@ -41,8 +41,9 @@ class Payment extends Controller
         $status = $request->input('status');
         $payment_id = $request->input('payment_id');
         $address_in = $request->input('address_in');
+        $responsedata= $request->all();
+        $transactionId = $responsedata['txid_in'];
         if(!empty($address_in)){
-            $responsedata= $request->all();
             Log::channel("creditcardpayissa")->info('Payment callback Response: '.json_encode($responsedata));
             $paymentLog = PaymentLog::where('id', $payment_id)->with('user')->first();
             if(!$paymentLog){
@@ -57,8 +58,8 @@ class Payment extends Controller
                     'payment_status' => 'success',
                 ]);
                 $email = $paymentLog->initiated_by;
-                $amount = $responsedata['value_coin'];
-                $transactionId = $responsedata['txid_in'];
+                $amount = $responsedata['amount'];
+
 
                 $settings = settings();
                 $this->api->SetLoggerWriteDebug(config('constants.IS_WRITE_DEBUG_LOG'));
@@ -142,7 +143,9 @@ class Payment extends Controller
 
                 $payment_res = json_encode($request->all());
                 $paymentLog = PaymentLog::where(DB::raw('payment_id'), $payment_id)->with('user')->first();
-                if ($status == "success") {
+                $account = Account::where('id',$paymentLog->account_id)->first();
+
+                if ($status == "success" && $account) {
                     // Get the payment log
                     if ($paymentLog && strtolower($paymentLog->payment_status) != "success") {
                         // Update payment log
@@ -153,23 +156,48 @@ class Payment extends Controller
                         $email = $paymentLog->initiated_by;
                         $amount = $paymentLog->payment_amount;
                         // Create a new wallet deposit
-                        $walletDeposit = WalletDeposit::create([
-                            'email' => $email,
-                            'deposit_amount' => $amount,
-                            'deposit_type' => "Now Payment",
-                            'currency_type' => "USD",
-                            'status' => 1,
-                        ]);
-
-                        if ($walletDeposit) {
-                            $this->sendSuccessEmail($email, $amount, $paymentLog,$walletDeposit->id);
-                            $this->subscribeToKlaviyoList($paymentLog->user,$amount,$subscribeToKlaviyoList);
-                            return redirect('/wallet_deposit')->with('success', "Successfully Deposited \$$amount To Your Wallet");
+                        // $walletDeposit = WalletDeposit::create([
+                        //     'email' => $email,
+                        //     'deposit_amount' => $amount,
+                        //     'deposit_type' => "Now Payment",
+                        //     'currency_type' => "USD",
+                        //     'status' => 1,
+                        // ]);
+                        $errorCode = $this->api->TradeBalance($account->code, $typed = MTEnDealAction::DEAL_BALANCE, $amount, $comment, $ticket, $margin_check = true);
+                        if ($errorCode != MTRetCode::MT_RET_OK) {
+                            $error = MTRetCode::GetError($errorCode);
+                            return response()->json([
+                                'success' => false,
+                                'message' => 'Something went wrong',
+                                'error' => $error,
+                            ], 400);
                         } else {
-                            return redirect('/wallet_deposit')->with('error', "Something went wrong. Please Try Again");
+                            $tradeDeposit = TradeDeposit::create([
+                                'user_id' => $paymentLog->user_id,
+                                'account_id' => $paymentLog->account_id,
+                                'email' => $email,
+                                'code' => $account->code,
+                                'deposit_amount' => $amount,
+                                'deposit_type' => 'CreditCardPayissa',
+                                'deposit_from' => 'CreditCardPayissa',
+                                'status' => 1,
+                                'deposit_currency' => 'USD',
+                                'transaction_id' => $transactionId,
+                                'deposted_date' => now(),
+                                'callback_data' => json_encode($responsedata),
+                                'callback_code' => "success",
+                            ]);
+                        }
+
+                        if ($tradeDeposit) {
+                            $this->sendSuccessEmail($email, $amount, $paymentLog,$tradeDeposit->id);
+                            $this->subscribeToKlaviyoList($paymentLog->user,$amount,$subscribeToKlaviyoList);
+                            return redirect('/trade-deposit')->with('success', "Successfully Deposited \$$amount To Your Wallet");
+                        } else {
+                            return redirect('/trade-deposit')->with('error', "Something went wrong. Please Try Again");
                         }
                     } else {
-                        return redirect('wallet_deposit')->with('error', "Payment already processed or invalid.");
+                        return redirect('trade-deposit')->with('error', "Payment already processed or invalid.");
                     }
                 } else {
                     // Update payment log for failed payment
@@ -177,7 +205,7 @@ class Payment extends Controller
                         'payment_res' => $payment_res,
                         'payment_status' => $status,
                     ]);
-                    return redirect('/wallet_deposit')->with('error', "Payment Failed: Something Went Wrong. Please try again");
+                    return redirect('/trade-deposit')->with('error', "Payment Failed: Something Went Wrong. Please try again");
                 }
 
             }
