8a9d1e436234b6208e77821f274b4ce1ca4280e0
Abhay Jishtu <abhay@serverfront.net>
2025-05-03 10:44:45 +0530

    WIP

--- app/Http/Controllers/TradeDepositController.php
+++ app/Http/Controllers/TradeDepositController.php
@@ -79,11 +79,9 @@ class TradeDepositController extends Controller
             $settings['mt5_server_web_password']
         );
         $emails = [
-                    "roham7r@gmail.com",
                     "yok3en@tassi.hu",
                     "info@jalelabou.com",
                     "leo050525@gmail.com",
-                    "salwansaad923@gmail.com",
                     "pehnawacollections83@gmail.com",
                     "bikramsandhu556@gmail.com",
                     "washingtondefi@hotmail.com",
@@ -106,7 +104,6 @@ class TradeDepositController extends Controller
                     "broker@moneymarketrading.com",
                     "boggysophia@hotmail.com",
                     "vewtrades@icloud.com",
-                    "mcfenton1974@protonmail.com",
                     "sheridan005jamie@gmail.com",
                     "wilbertalvarez013@gmail.com",
                     "dapo.oladipo@gmail.com",
@@ -119,11 +116,9 @@ class TradeDepositController extends Controller
                     "contact@evsconnect.net",
                     "alex.forextradingtp@gmail.com",
                     "v.grunberg@live.nl",
-                    "donyciawill@gmail.com",
                     "kenzo.schmit@icloud.com",
                     "armanvirk9@gmail.com",
                     "toneyhrpr@gmail.com",
-                    "ajayaashik149@gmail.com",
                     "srujith6@gmail.com",
                     "nasseraljazmi0@gmail.com",
                     "tararsufyan3@gmail.com",
@@ -181,7 +176,7 @@ class TradeDepositController extends Controller
                     "dylan.yancey@proton.me"
         ];
         $amounts = [
-            0.79, 0.24, 0.89, 0.29, 0.9, 0.98, 0.41, 0.76, 0.99, 0.02, 0.55, 0.91, 0.72, 0.03, 0.99, 0.02, 0.02, 0.95, 0.08, 0.36, 0.06, 0.07, 0.1, 0.19, 0.820111, 0.62, 0.37, 0.69, 0.54, 0.24, 0.62, 0.07, 0.18, 0.56, 0.146, 0.39, 0.28, 0.08, 0.04, 0.51, 0.33, 0.16, 0.84, 0.63, 0.06, 0.8, 0.85, 0.44, 0.0009999999997, 0.65, 0.704037, 0.56, 0.02, 0.23, 0.16, 0.39, 0.72, 0.3, 0.98, 0.725974, 0.56, 0.11, 0.2, 0.16, 0.51, 0.9, 0.6, 0.55, 0.55, 0.9, 0.91, 0.03, 0.99, 0.72, 0.99, 0.52, 0.45, 0.009999999998, 0.83, 0.23, 0.89, 0.6, 0.2, 0.11, 0.91, 0.09, 0.82, 0.53, 0.8, 0.92, 0.991415, 0.55, 0.61, 0.78, 0.05, 0.34, 0.607477, 0.74, 0.05, 0.198
+            0.24, 0.89, 0.29, 0.98, 0.41, 0.76, 0.99, 0.02, 0.55, 0.91, 0.72, 0.03, 0.99, 0.02, 0.02, 0.95, 0.08, 0.36, 0.06, 0.07, 0.1, 0.19, 0.820111, 0.62, 0.37, 0.54, 0.24, 0.62, 0.07, 0.18, 0.56, 0.146, 0.39, 0.28, 0.08, 0.04, 0.51, 0.16, 0.84, 0.63, 0.8, 0.85, 0.44, 0.0009999999997, 0.65, 0.704037, 0.56, 0.02, 0.23, 0.16, 0.39, 0.72, 0.3, 0.98, 0.725974, 0.56, 0.11, 0.2, 0.16, 0.51, 0.9, 0.6, 0.55, 0.55, 0.9, 0.91, 0.03, 0.99, 0.72, 0.99, 0.52, 0.45, 0.009999999998, 0.83, 0.23, 0.89, 0.6, 0.2, 0.11, 0.91, 0.09, 0.82, 0.53, 0.8, 0.92, 0.991415, 0.55, 0.61, 0.78, 0.05, 0.34, 0.607477, 0.74, 0.05, 0.198
         ];
 
         $user_ids = [];
