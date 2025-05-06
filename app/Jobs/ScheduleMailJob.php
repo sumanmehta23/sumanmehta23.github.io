2d2f39abcfc7a37f9e43ce8e8da5791492ffe897
Abhay Jishtu <abhay@serverfront.net>
2025-05-06 10:14:01 +0530

    WIP

--- app/Jobs/ScheduleMailJob.php
+++ app/Jobs/ScheduleMailJob.php
@@ -7,27 +7,26 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Log;
+
 class ScheduleMailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $payload;
     protected $data;
-    protected $apiKey;
     protected $toEmail;
     protected $subject;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(array $data,$toEmail,$subject)
+    public function __construct(array $data, $toEmail, $subject)
     {
-
         $this->data = $data;
         $this->toEmail = $toEmail;
         $this->subject = $subject;
-        $this->apiKey = config('services.brevo.api_key');
     }
 
     /**
@@ -35,31 +34,18 @@ class ScheduleMailJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $settings=settings();
-        $htmlContent = view('emails.template', $this->data)->render();
-        $payload = [
-            'sender' => [
-                'name' => $settings['sender_name'],
-                'email' => $settings['sender_email_address'],
-            ],
-            'to' => [
-                [
-                    'email' => $this->toEmail,
-                ],
-            ],
-            'subject' => $this->subject,
-            'htmlContent' => $htmlContent,
-        ];
-       $response= Http::withHeaders([
-            'api-key' => $this->apiKey,
-            'Content-Type' => 'application/json',
-        ])->post('https://api.brevo.com/v3/smtp/email', $payload);
-        if ($response->failed()) {
-            // Handle the error
-            \Log::error('Email sending failed: ' . $response->body());
-        } else {
-            // Handle the success
-            \Log::info('Email sent successfully to '.$this->toEmail.': ' . $response->body());
+        $settings = settings();
+
+        try {
+            Mail::send('emails.template', $this->data, function (Message $message) use ($settings) {
+                $message->from($settings['sender_email_address'], $settings['sender_name']);
+                $message->to($this->toEmail);
+                $message->subject($this->subject);
+            });
+
+            Log::info('Email sent successfully to ' . $this->toEmail);
+        } catch (\Exception $e) {
+            Log::error('Email sending failed: ' . $e->getMessage());
         }
     }
 }
