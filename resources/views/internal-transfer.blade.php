3b25009f28a9d497ec0ff47cdee3ffcd99a7c25b
Abhay Jishtu <abhay@serverfront.net>
2025-05-16 09:37:38 +0530

    WIP

--- resources/views/internal-transfer.blade.php
+++ resources/views/internal-transfer.blade.php
@@ -38,7 +38,7 @@
                                                             @foreach ($liveaccount_details as $acc)
                                                                 @php
                                                                     if ($acc->email == 'tammaru@gmail.com') {
-                                                                        dd($acc);
+                                                                        dump($acc);
                                                                     }
                                                                 @endphp
 
