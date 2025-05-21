7a481fb68837927ba978ebcc93c7b6e909dd0ac5
Abhay Jishtu <abhay@serverfront.net>
2025-05-21 16:52:38 +0530

    WIP

---
+++ app/Models/Trade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Trade extends Model
+{
+    use HasFactory,HasUuids;
+
+    protected $table = 'trades';
+
+    protected $guarded = [];
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+}
