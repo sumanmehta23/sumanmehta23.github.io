a88609cf4f35aa3e51029a799647850faeb710f7
Abhay Jishtu <abhay@serverfront.net>
2025-05-28 15:27:03 +0530

    WIP

--- app/Jobs/SyncTrades.php
+++ app/Jobs/SyncTrades.php
@@ -11,6 +11,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Bus\Batchable;
 
 class SyncTrades implements ShouldQueue
 {
