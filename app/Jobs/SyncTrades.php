376cc0e1fe41dcd0471b3bdd63b5c59e34b42723
Abhay Jishtu <abhay@serverfront.net>
2025-06-10 15:52:29 +0530

    WIP

--- app/Jobs/SyncTrades.php
+++ app/Jobs/SyncTrades.php
@@ -126,7 +126,6 @@ class SyncTrades implements ShouldQueue
                 //     $this->processBatch($tradesToUpsert);
                 //     $tradesToUpsert = [];
                 // }
-                dd($positionOrders);
 
                 Log::warning("position order {$positionOrders->count()} ");
                 Log::warning("existing trade {$existingTrade} ");
@@ -144,6 +143,9 @@ class SyncTrades implements ShouldQueue
                         $closedTradeData['id'] = $existingTrade->id;
                         $tradesToUpsert[] = $closedTradeData;
                     } else {
+                        $closedTradeData = $this->prepareClosedTrade($account, $positionId, $positionOrders->first(), $positionOrders->last());
+                        $closedTradeData['id'] = $existingTrade->id;
+                        $tradesToUpsert[] = $closedTradeData;
                         // No open trade exists; you can choose to skip or insert fresh closed trade.
                         // To strictly follow your requirement, we will SKIP it:
                         Log::warning("Closed trade found for position_id {$positionId} but no matching open trade exists in DB. Skipping.");
