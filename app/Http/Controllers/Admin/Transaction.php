a2652a66959e7abbf5156922c5262166126a9ceb
Abhay Jishtu <abhay@serverfront.net>
2025-06-16 09:35:42 +0530

    WIP

--- app/Http/Controllers/Admin/Transaction.php
+++ app/Http/Controllers/Admin/Transaction.php
@@ -441,6 +441,10 @@ class Transaction extends Controller
             }else{
                 $client_wallet='';
             }
+            if($details->email == 'yunusasani407@gmail.com'){
+                dd($details);
+            }
+
             return view('admin.trading_withdrawal_details', compact('details','client_wallet'));
         }
     }
