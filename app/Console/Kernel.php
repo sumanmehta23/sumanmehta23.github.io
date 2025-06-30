7e6764ac40ddbcbdce2a27a67893ec210556241e
Abhay Jishtu <abhay@serverfront.net>
2025-06-30 16:31:53 +0530

    WIP

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,14 +16,14 @@ class Kernel extends ConsoleKernel
         $schedule->command('telescope:prune')->daily();
 
         $schedule->command('app:activate-competition-accounts')->everyFiveSeconds();
-        $schedule->command('app:sync-trades')->everyFiveMinutes();
+        $schedule->command('app:sync-trades')->everyFiveSeconds();
 
         // $schedule->command('app:breach-account')->monthlyOn(1, '00:00');
 
         $schedule->command('app:breach-account')->everyFiveSeconds();
 
 
-        $schedule->command('app:sync-accounts')->everyFiveMinutes();
+        $schedule->command('app:sync-accounts')->everyFiveSeconds();
         $schedule->command('app:sync-daily-reports')->daily();
         // $schedule->command('app:sync-account-trades')->everyTwoHours();
         // $schedule->command('app:update-price-snapshots')->hourly();
