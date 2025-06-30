fcf3f90551c5b4a93d1555798b9d0c94d38ec138
Abhay Jishtu <abhay@serverfront.net>
2025-06-30 12:58:44 +0530

    WIP

---
+++ database/migrations/2025_06_30_065502_change_competition_dates_to_datetime_in_account_types_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('account_types', function (Blueprint $table) {
+            $table->dateTime('competition_start_date')->nullable()->change();
+            $table->dateTime('competition_end_date')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('account_types', function (Blueprint $table) {
+            $table->date('competition_start_date')->change();
+            $table->date('competition_end_date')->change();
+        });
+    }
+};
