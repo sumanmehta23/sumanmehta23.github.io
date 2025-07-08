776ec1469040a4a83596e814e21c57f61edea9de
Abhay Jishtu <abhay@serverfront.net>
2025-07-08 10:38:47 +0530

    WIP

--- resources/views/admin/ui_settings.blade.php
+++ resources/views/admin/ui_settings.blade.php
@@ -127,9 +127,9 @@
                                     <input type="text" class="form-control" name="mt5_server_web_login"
                                         value="{{ $settings['mt5_server_web_login'] }}" required>
                                 </div>
-                                <div class="mb-0">
+                                <div class="mb-0" hidden>
                                     <label class="form-label">MT5 Server Web Password</label>
-                                    <input type="password" class="form-control" name="mt5_server_web_password"
+                                    <input type="text" class="form-control" name="mt5_server_web_password"
                                         value="{{ $settings['mt5_server_web_password'] }}" required>
                                 </div>
                             </div>
