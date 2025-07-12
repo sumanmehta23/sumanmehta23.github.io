5c949e3a65c8edb25c4a05a5b73f30217a74475b
Abhay Jishtu <abhay@serverfront.net>
2025-07-12 10:07:57 +0530

    WIP

--- app/Jobs/ScheduleMailJob.php
+++ app/Jobs/ScheduleMailJob.php
@@ -29,7 +29,7 @@ class ScheduleMailJob implements ShouldQueue
         $this->toEmail = $toEmail;
         $this->subject = $subject;
         $this->apiKey = config('services.brevo.api_key');
-        dd($this);
+        // dd($this);
     }
 
     /**
