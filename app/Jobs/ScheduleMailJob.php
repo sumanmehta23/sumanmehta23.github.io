2bba3801bf1aae3e431ce069cec8801498a7fcc9
Abhay Jishtu <abhay@serverfront.net>
2025-07-12 16:23:53 +0530

    WIP

--- app/Jobs/ScheduleMailJob.php
+++ app/Jobs/ScheduleMailJob.php
@@ -58,8 +58,14 @@ class ScheduleMailJob implements ShouldQueue
                 $template = 'emails.transactionApproved';
             }elseif(strpos($this->subject, 'Fund Deposit') !== false){
                 $template = 'emails.fundsAdd';
+            }elseif(strpos($this->subject, 'Live Account Details') !== false){
+                $template = 'emails.issueLiveAccount';
+            }elseif(strpos($this->subject, 'Password Reset') !== false){
+                $template = 'emails.resetPassword';
             }else{
-                $template = 'emails.template';
+
+                $template = 'emails.defaultTemplate';
+                // $template = 'emails.template';
             }
 
             if ($maildriver == 'brevo') {
