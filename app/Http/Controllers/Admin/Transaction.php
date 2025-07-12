3dc60be0d1e59f7ff4ebb73e2f95fc10eee0dd7d
Abhay Jishtu <abhay@serverfront.net>
2025-07-12 09:11:32 +0530

    WIP

--- app/Http/Controllers/Admin/Transaction.php
+++ app/Http/Controllers/Admin/Transaction.php
@@ -507,7 +507,7 @@ class Transaction extends Controller
             $content = '<div>We are pleased to inform you that your transaction has been successfully approved manually.</div>
                         <div>The approved amount has been withdrawn from your wallet.</div>
                         <div><b>Transaction Details</b></div>
-                        <div><b>Approved Amount: </b>$' . $deposit_details->withdraw_amount . '</div>
+                        <div><b>Approved Amount: </b>$' . $deposit_details->withdrawal_amount . '</div>
                         <div><b>Transaction ID: </b>' . $transid . '</div>
                         <div><b>Withdrawal Date: </b>' . $deposit_details->withdraw_date . '</div>
                         <div><b>Withdrawal Type: </b>' . $deposit_details->withdraw_type . '</div>';
