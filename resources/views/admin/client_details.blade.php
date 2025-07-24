550ab2f93afe7f16e177c55dbdb2dfd802400e7e
Abhay Jishtu <abhay@serverfront.net>
2025-07-24 12:03:52 +0530

    WIP

--- resources/views/admin/client_details.blade.php
+++ resources/views/admin/client_details.blade.php
@@ -890,10 +890,10 @@
                                                                 <div class="card-title">INTRODUCING BROKER</div>
                                                                 <div>
                                                                     {{-- {{ dd($user->ib->status) }} --}}
-                                                                    <?php if ($user->ib->status == 0): ?>
+                                                                    <?php if ($user->ib && $user->ib->status == 0): ?>
                                                                     <span
                                                                         class="badge bg-outline-warning text-end">Pending</span>
-                                                                    <?php elseif ($user->ib->status == 1): ?>
+                                                                    <?php elseif ($user->ib && $user->ib->status == 1): ?>
                                                                     <span class="badge bg-outline-success text-end">Active
                                                                         IB</span>
                                                                     <?php else: ?>
