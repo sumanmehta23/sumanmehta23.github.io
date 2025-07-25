a41bc8f8882ef049b3217fd4a82bce704f0ff4cc
Abhay Jishtu <abhay@serverfront.net>
2025-07-25 16:16:55 +0530

    WIP

--- app/Http/Controllers/TradeWithdrawal.php
+++ app/Http/Controllers/TradeWithdrawal.php
@@ -425,6 +425,7 @@ class TradeWithdrawal extends Controller
                                 }
 
                                 // $leverage = round($account->leverage * (100 / ($trade_user->Balance + $trade_user->Credit)), 2);
+                                Log::alert("mt5account->Leverage : " . json_encode($mt5account));
                                 Log::alert(" $mt5account->Leverage * ($amount / ($trade_user->Balance + $trade_user->Credit)) ");
 
                                 $leverage = round($mt5account->Leverage * (($amount / ($trade_user->Balance + $trade_user->Credit))),2);
