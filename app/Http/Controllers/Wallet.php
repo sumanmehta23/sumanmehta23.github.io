ffd171bf839a253c0c55b87d86f882cf05466891
Abhay Jishtu <abhay@serverfront.net>
2025-07-25 12:42:57 +0530

    WIP

--- app/Http/Controllers/Wallet.php
+++ app/Http/Controllers/Wallet.php
@@ -1222,7 +1222,7 @@ class Wallet extends Controller
                                 return redirect()->back()->with('error', 'Something went wrong on Updating leverage' . MTRetCode::GetError($error_code));
                             }
 
-                            $leverage = round($account->leverage * (100 / ($bonus_amount + ($trade_user->Balance + $trade_user->Credit))),2);
+                            $leverage = round($account->leverage * (($bonus_amount / ($trade_user->Balance + $trade_user->Credit))),2);
                             $trade_user->Leverage = $leverage;
 
                             $updated_user = "";
