b64e834816f9e80402915cf5cc8ce299529630d9
Abhay Jishtu <abhay@serverfront.net>
2025-08-08 15:49:55 +0530

    WIP

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Site Under Maintenance</title>
+    <link rel="icon" href="{{ asset($settings['favicon']) }}" >
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         body {
