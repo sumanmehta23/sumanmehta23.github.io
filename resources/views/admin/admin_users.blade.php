e7bac32683b275d5169b8580b1790ba9ccf1e96f
Abhay Jishtu <abhay@serverfront.net>
2025-08-12 15:54:07 +0530

    WIP

--- resources/views/admin/admin_users.blade.php
+++ resources/views/admin/admin_users.blade.php
@@ -121,7 +121,7 @@
         </div>
 
         <div class="page-header">
-            <h1 class="page-title">Adminss Users</h1>
+            <h1 class="page-title">Admin Users</h1>
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="javascript:void(0);">Dashboard</a></li>
               <li class="breadcrumb-item active" aria-current="page">Admin Users</li>
