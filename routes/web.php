f7fabea3490ec1f140a290044637ff4dfaee7906
Abhay Jishtu <abhay@serverfront.net>
2025-08-25 11:49:22 +0530

    WIP

--- routes/web.php
+++ routes/web.php
@@ -165,10 +165,10 @@ Route::get("/se", function (SubscribeToKlaviyoList $subscribeToKlaviyoList) {
 Route::post('/user/kyc/listener', [KycController::class, 'listener'])->name('kyc.listener');
 Route::get('/payment-response', [Payment::class, 'handlePaymentResponse'])->name('handlePaymentResponse');
 
-Route::get('/failed-payment-response', [Payment::class, 'handleFailedPaymentResponse'])->name('handleFailedPaymentResponse');
+// Route::get('/failed-payment-response', [Payment::class, 'handleFailedPaymentResponse'])->name('handleFailedPaymentResponse');
 Route::get('/handle-failed-payment-response', [Payment::class, 'handleFailedPaymentResponse'])->name('handleFailedPaymentResponse');
 
-// Route::get('/manually-payment-response', [Payment::class, 'manuallyPaymentResponse'])->name('manuallyPaymentResponse');
+Route::get('/manually-payment-response', [Payment::class, 'manuallyPaymentResponse'])->name('manuallyPaymentResponse');
 
 Route::post('/paymentcallback', [PaymentCallbackController::class, 'handleCallback'])->name('paymentcallback');
 Route::get('/', [LoginController::class, 'showLoginForm'])->name('login_index');
