d08e30ab7ad7d5e531631b9ecd2198a9cd90e92c
Digvijay Singh <tech@lqhmarkets.com>
2025-09-06 15:17:00 +0530

    refactor: Update order filtering to use object properties in BatchSyncTradesJob

--- app/Jobs/BatchSyncTradesJob.php
+++ app/Jobs/BatchSyncTradesJob.php
@@ -272,8 +272,8 @@ class BatchSyncTradesJob implements ShouldQueue
 
             // Phase 5: Data Processing - Orders Grouping
             $phaseStart = microtime(true);
-            $ordersByPosition = collect($orders)->filter(fn($order) => $order['ExpertPositionID'] > 0)
-                ->groupBy('ExpertPositionID');
+            $ordersByPosition = collect($orders)->filter(fn($order) => $order->ExpertPositionID > 0)
+                ->groupBy(fn($order) => $order->ExpertPositionID);
             $tradesToUpsert = [];
             $savedCount = 0;
             $timings['orders_processing'] = round((microtime(true) - $phaseStart) * 1000, 2);
