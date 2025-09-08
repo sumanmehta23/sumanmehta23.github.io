c2943c15e67f3461b9c4e2792213a6d33648c5a4
Abhay Jishtu <abhay@serverfront.net>
2025-09-08 17:07:16 +0530

    WIP

--- app/Console/Commands/SyncAccountTrades.php
+++ app/Console/Commands/SyncAccountTrades.php
@@ -25,7 +25,7 @@ class SyncAccountTrades extends Command
         $totalJobsCreated = 0;
         Ib1::with(['planDetails', 'user'])  // Eager load related models
             ->where('status', 1)
-            ->where('email', 'hautranrcb@gmail.com')
+            // ->where('email', 'hautranrcb@gmail.com')
             ->whereNotNull('ib_plan_details_id')
             ->cursor()  // More memory efficient for large datasets
             ->each(function ($ib1) use ($batchSize, $maxJobs, $activeOnly, &$totalJobsCreated) {
