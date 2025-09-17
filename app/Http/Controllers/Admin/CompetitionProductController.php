a132e131a27676eadd2c345a3c2e361a3d8b890a
Abhay Jishtu <abhay@serverfront.net>
2025-09-17 13:47:06 +0530

    WIP

--- app/Http/Controllers/Admin/CompetitionProductController.php
+++ app/Http/Controllers/Admin/CompetitionProductController.php
@@ -24,7 +24,6 @@ class CompetitionProductController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
         try {
             $validatedData = $request->validate([
                 'groupCreation' => ['required'],
