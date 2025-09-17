074845cf16780e4e6136bf731b63935522cf513c
Abhay Jishtu <abhay@serverfront.net>
2025-09-17 16:44:06 +0530

    WIP

--- app/Http/Controllers/CompetitionController.php
+++ app/Http/Controllers/CompetitionController.php
@@ -157,6 +157,10 @@ class CompetitionController extends Controller
         $start_date = $group->competition_start_date;
         $end_date = $group->competition_end_date;
 
+        dump($start_date);
+        dump($end_date);
+        dd(now());
+
         if ($end_date < now()) {
             return redirect()->back()->with('error', 'Competition is over, try another.');
         }
