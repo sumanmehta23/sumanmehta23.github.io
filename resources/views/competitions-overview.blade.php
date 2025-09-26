17abf05576f72d755e2ac9c16e8d86e3b7d8a3d9
Abhay Jishtu <abhay@serverfront.net>
2025-09-26 16:23:25 +0530

    WIP

--- resources/views/competitions-overview.blade.php
+++ resources/views/competitions-overview.blade.php
@@ -31,13 +31,19 @@
     <!-- Navbar -->
     <header class="flex flex-col md:flex-row justify-between items-center p-6 bg-white shadow">
         <div class="flex items-center justify-center md:justify-start space-x-2 mb-3 md:mb-0">
-            <img src="/{{ $settings['admin_sidebar_logo'] }}" class="w-36 md:w-44" alt="logo">
+            <img src="/{{ $settings['admin_sidebar_logo'] }}" href='https://www.lqhmarkets.com/' class="w-36 md:w-44" alt="logo">
         </div>
         <nav class="flex flex-col md:flex-row items-center gap-3 md:space-x-6">
             <a href="/competitions-overview" class="text-gray-700 hover:text-emerald-600 font-medium">Competitions</a>
-            <a href="/login" class="px-5 py-2 bg-emerald-700 text-white rounded-lg font-semibold shadow hover:bg-emerald-800 w-full md:w-auto text-center">
-                Sign Up
-            </a>
+            @guest
+                <a href="/login" class="px-5 py-2 bg-emerald-700 text-white rounded-lg font-semibold shadow hover:bg-emerald-800 w-full md:w-auto text-center">
+                    Sign Up
+                </a>
+            @else
+                <a href="/dashboard" class="px-5 py-2 bg-emerald-700 text-white rounded-lg font-semibold shadow hover:bg-emerald-800 w-full md:w-auto text-center">
+                    Dashboard
+                </a>
+            @endguest
         </nav>
     </header>
 
