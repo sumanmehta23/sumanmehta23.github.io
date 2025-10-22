7d6344678e8e058adb56807748e05a70136ac59b
mirzatradetech <mirza@tradetech.app>
2025-10-23 01:03:34 +0500

    encrypted fields revert back

--- app/Models/ClientWallet.php
+++ app/Models/ClientWallet.php
@@ -14,11 +14,6 @@ class ClientWallet extends Model
     protected $table = 'client_wallets';
     protected $guarded = [];
 
-    // Hide encrypted fields from JSON serialization to prevent MAC errors
-    protected $hidden = [
-        'wallet_address',
-    ];
-
     public function casts(){
         return [
             'wallet_address' => 'encrypted',
