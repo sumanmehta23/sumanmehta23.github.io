3a799dc7f19d89355e2325a6fcb1e015df104c9c
Abhay Jishtu <abhay@serverfront.net>
2025-12-09 12:21:07 +0530

    WIP

--- app/Services/BalanceSyncService.php
+++ app/Services/BalanceSyncService.php
@@ -181,7 +181,7 @@ class BalanceSyncService
                     ->where('bonus_type', 'Bonus In')
                     ->where('admin_remark', 'Promo Bonus')
                     ->when($account->bonus_payoff_sync_at, function ($query, $bonusPayoffSyncAt) {
-                        $query->where('bonus_date', '>=', $bonusPayoffSyncAt);
+                        $query->where('bonus_date', '<=', $bonusPayoffSyncAt);
                     });
 Log::info('Promo Bonus Query: '.$accountPromoBonus->toSql(), $accountPromoBonus->getBindings());
         Log::info("account sync for payoff".$account->code);
