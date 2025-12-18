acab73b351ce23ee9202dde84da13a2f64a55b5e
Abhay Jishtu <abhay@serverfront.net>
2025-12-18 17:15:11 +0530

    WIP

--- app/Http/Controllers/Wallet.php
+++ app/Http/Controllers/Wallet.php
@@ -332,13 +332,19 @@ class Wallet extends Controller
         $ClientWallet = ClientWallet::where('user_id', $user->id)
             ->latest('created_at') // Specify the column to order by
             ->first();
+
         $content =
             '<p>Welcome to ' . htmlspecialchars($settings['admin_title'], ENT_QUOTES, 'UTF-8') . '!</p>' .
-            '<p></p>' .
             '<p>You are receiving this email because you have added a new wallet address to your account.</p>' .
-            '<p></p>' .
-            '<p>Wallet Address: ' . $request->wallet_address . ' </p>' .
-            '<p></p>' .
+            '<p><strong>Wallet Address:</strong><br>' .
+            '<span style="
+                font-family: monospace;
+                word-break: break-all;
+                overflow-wrap: break-word;
+                white-space: normal;
+                display: inline-block;
+                max-width: 100%;
+            ">' . e($request->wallet_address) . '</span></p>' .
             '<p>Click the link below to activate your Wallet Address</p>';
 
         $templateVars = [
@@ -381,15 +387,19 @@ class Wallet extends Controller
 
         $walletAddress = htmlspecialchars($request->wallet_address, ENT_QUOTES, 'UTF-8');
 
-        $content = '
-            <p>Welcome to ' . htmlspecialchars($settings['admin_title'], ENT_QUOTES, 'UTF-8') . '!</p>
-            <p></p>
-            <p>You are receiving this email because you have added a new wallet address to your account.</p>
-            <p></p>
-            <p>Wallet Address: ' . $walletAddress . '</p>
-            <p></p>
-            <p>Click the link below to activate your Wallet Address:</p>
-        ';
+        $content =
+            '<p>Welcome to ' . htmlspecialchars($settings['admin_title'], ENT_QUOTES, 'UTF-8') . '!</p>' .
+            '<p>You are receiving this email because you have added a new wallet address to your account.</p>' .
+            '<p><strong>Wallet Address:</strong><br>' .
+            '<span style="
+                font-family: monospace;
+                word-break: break-all;
+                overflow-wrap: break-word;
+                white-space: normal;
+                display: inline-block;
+                max-width: 100%;
+            ">' . e($request->wallet_address) . '</span></p>' .
+            '<p>Click the link below to activate your Wallet Address</p>';
 
         $templateVars = [
             'name' => $user->fullname,
