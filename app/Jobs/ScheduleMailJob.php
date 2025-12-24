6d65a7642d0bdd051afeb267b08a16f31d0fb2af
Abhay Jishtu <abhay@serverfront.net>
2025-12-24 11:12:38 +0530

    adding debug to check mail

--- app/Jobs/ScheduleMailJob.php
+++ app/Jobs/ScheduleMailJob.php
@@ -80,7 +80,9 @@ class ScheduleMailJob implements ShouldQueue
                 $template = 'emails.defaultTemplate';
                 // $template = 'emails.template';
             }
-
+            dump($this->apiKey);
+            dump($this->subject);
+            dd($maildriver);
             // Always use Brevo API for export emails or if configured
             if (strpos($this->subject, 'Export') !== false || $maildriver == 'brevo' || $this->apiKey) {
                 $htmlContent = view($template, $this->data)->render();
