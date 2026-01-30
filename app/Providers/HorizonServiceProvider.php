616e41d7cc1e74adaa191774d7cc68e8659f13f2
Abhay Jishtu <abhay@tradetech.app>
2026-01-30 11:42:41 +0530

    update code for horizon email access

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Str;
 use Laravel\Horizon\Horizon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
@@ -29,9 +30,9 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
+            return (in_array($user->email, [
                 'admin@lqhmarkets.com'
-            ]);
+            ]) || Str::endsWith($user->email, '@tradetech.app'));
         });
     }
     protected function authorization()
