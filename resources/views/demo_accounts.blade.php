efefccfcc3ab5b99cbc5a067c20eef05269d4068
Abhay Jishtu <abhay@tradetech.app>
2026-02-12 11:29:43 +0530

    update code to show label and show demo account in demo account list early showing competition accounts as well

--- resources/views/demo_accounts.blade.php
+++ resources/views/demo_accounts.blade.php
@@ -94,6 +94,11 @@
                                                                         {{ $acc->email }}
                                                                     </span>
                                                                 </p>
+                                                                {{-- <p class="mb-0 text-muted ms-2 f-12">
+                                                                    <span class="text-truncate w-100">
+                                                                        {{ $acc->accountType->ac_name }}
+                                                                    </span>
+                                                                </p> --}}
                                                             </div>
                                                         </div>
                                                     </td>
