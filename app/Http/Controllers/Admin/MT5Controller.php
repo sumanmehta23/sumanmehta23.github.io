4871c0b9cffb8f1296cef34860fc05f7590766f1
Abhay Jishtu <abhay@tradetech.app>
2026-03-11 13:04:25 +0530

    update code

--- app/Http/Controllers/Admin/MT5Controller.php
+++ app/Http/Controllers/Admin/MT5Controller.php
@@ -648,7 +648,7 @@ class MT5Controller extends Controller
             $account->deletion_type = 'soft';
             $account->save();
             $account->delete();
-            return redirect()->route('admin.dashboard')->with('success', 'X9 Account Soft Deleted Successfully');
+            return redirect()->back()->with('success', 'X9 Account Soft Deleted Successfully');
         } elseif ($platform === 'mt5') {
             $trade_user = NULL;
             if (($error_code = $this->api->UserGet($login, $trade_user) != MTRetCode::MT_RET_OK)) {
@@ -670,7 +670,7 @@ class MT5Controller extends Controller
             $account->deletion_type = 'soft';
             $account->save();
             $account->delete();
-            return redirect()->route('admin.dashboard')->with('success', 'MT5 Account Soft Deleted Successfully');
+            return redirect()->back()->with('success', 'MT5 Account Soft Deleted Successfully');
         }
     }
 
@@ -778,7 +778,7 @@ class MT5Controller extends Controller
             $account->deletion_type = null;
             $account->save();
             $account->restore();
-            return redirect()->route('admin.dashboard')->with('success', 'X9 Account Restored Successfully');
+            return redirect()->back()->with('success', 'X9 Account Restored Successfully');
         } elseif ($platform === 'mt5') {
             // $trade_user = NULL;
             // if (($error_code = $this->api->UserGet($login, $trade_user) != MTRetCode::MT_RET_OK)) {
@@ -802,7 +802,7 @@ class MT5Controller extends Controller
             $account->deletion_type = null;
             $account->save();
             $account->restore();
-            return redirect()->route('admin.dashboard')->with('success', 'MT5 Account Restored Successfully');
+            return redirect()->back()->with('success', 'MT5 Account Restored Successfully');
         }
     }
 
