7e525863e831a2d7b22d784688b6dc75e18d4666
Abhay Jishtu <abhay@tradetech.app>
2026-04-08 09:58:05 +0530

    update code to add support button intercom

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -195,8 +195,45 @@
                 'flagPreviewId' => 'flag-preview-client-login'
             ])
         </div>
+        <script>
+            window.intercomSettings = {
+                api_base: "https://api-iam.intercom.io",
+                app_id: "hcaolnkq"
+            };
+        </script>
+        <script>
+            (function () {
+                var w = window;
+                var ic = w.Intercom;
+                if (typeof ic === "function") {
+                    ic('reattach_activator');
+                    ic('update', w.intercomSettings);
+                } else {
+                    var d = document;
+                    var i = function () { i.c(arguments); };
+                    i.q = [];
+                    i.c = function (args) { i.q.push(args); };
+                    w.Intercom = i;
+                    var l = function () {
+                        var s = d.createElement('script');
+                        s.type = 'text/javascript';
+                        s.async = true;
+                        s.src = 'https://widget.intercom.io/widget/hcaolnkq';
+                        var x = d.getElementsByTagName('script')[0];
+                        x.parentNode.insertBefore(s, x);
+                    };
+                    if (document.readyState === 'complete') {
+                        l();
+                    } else if (w.attachEvent) {
+                        w.attachEvent('onload', l);
+                    } else {
+                        w.addEventListener('load', l, false);
+                    }
+                }
+            })();
 
-        @if (session('retry_after'))
+        </script>
+    @if (session('retry_after'))
         <script>
             (function() {
                 let retryAfter = {{ session('retry_after') }};
@@ -252,6 +289,6 @@
                 }
             })();
         </script>
-        @endif
+    @endif
 
 @endsection
