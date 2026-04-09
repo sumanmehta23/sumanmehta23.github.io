c9cc812b08717875fc4f45f680b15c85cc858de8
Shubham Sharma <shubham@tradetech.app>
2026-04-09 13:19:23 +0530

    Update error message for Commission Transfer

--- app/Http/Controllers/Ib.php
+++ app/Http/Controllers/Ib.php
@@ -416,7 +416,7 @@ class Ib extends Controller
                 }
             } else {
 
-                return redirect()->back()->with('error', 'Insufficient IB Transferrable Balance');
+                return redirect()->back()->with('error', 'Insufficient Transferable Balance');
             }
         }
     }
