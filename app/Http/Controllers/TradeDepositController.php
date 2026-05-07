ec5dd4e0bddae8c04e8d3bca741599fbb189543a
Digvijay Singh <tech@lqhmarkets.com>
2026-05-07 21:33:18 +0530

    fix: Correct Payissa checkout URL format in TradeDepositController

--- app/Http/Controllers/TradeDepositController.php
+++ app/Http/Controllers/TradeDepositController.php
@@ -663,7 +663,7 @@ class TradeDepositController extends Controller
                 'remarks' => $success_url,
             ]);
             $amount += (4 / 100) * $amount;
-            $url = config("services.payissa.checkouturl") . 'pay.php?address=' . $responsdata['address_in'] . "&amount=" . $amount . "&email=" . $user->email . "&currency=" . $currency;
+            $url = config("services.payissa.checkouturl") . '/pay.php?address=' . $responsdata['address_in'] . "&amount=" . $amount . "&email=" . $user->email . "&currency=" . $currency;
 
             return ['invoice_url' => $url];
         }
